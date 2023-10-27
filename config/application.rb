df222a5f5fb5b0fafa532aa251382ffd669e046f
Luiza Nunes <luiza.isabella@hotmail.com>
2023-10-27 15:52:03 -0300

    Adjusting front-end

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,10 @@ module StateApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.assets.paths << Rails.root.join('app', 'assets', 'site-images')
+
+    Dir.glob("#{Rails.root}/app/assets/images/**/").each do |path|
+      config.assets.paths << path
+    end
   end
 end
